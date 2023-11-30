f3d9cb100a58c2522e54c81709806c78ac33d69a
user <user@DESKTOP-U2N62I8>
2023-11-30 18:55:32 +0300

    Добавьте файлы проекта.

---
+++ lab2/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace lab2
+{
+    internal class Book
+    {
+        public string name_of_book;
+        public string autor;
+        public int ISBN;
+        public string annotation;
+        public string summary;
+        public string Text;
+        public int count = 0;
+        public bool flag = true;
+
+        public Book(string name_of_book, string autor, int ISBN, string annotation, string summary, string Text)
+        {
+            this.name_of_book =name_of_book;
+            this.autor =autor;
+            this.ISBN = ISBN;
+            this.annotation = annotation;
+            this.summary = summary;
+            this.Text = Text;
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+/*Информация о книге включает название, имя автора, жанры, дату публикации, аннотацию, ISBN.
+Из главного меню программы пользователь, посредством выбора одного из пронумерованных пунктов, может производить следующие операции:
+-добавление книги в каталог
+-выборку информации о конкретной книге по:
+--названию или его фрагменту
+--имени автора 
+--ISBN
+--ключевым словам
+-выход.
+Поиск по ключевым словам должен возвращать краткие описания книг (без аннотации) в таком порядке,
+чтобы в начале выборки оказывались книги c наибольшим количеством найденных ключевых слов, 
+а в конце – с наименьшим количеством. Если ключевое слово найдено в аннотации, это должно быть отражено в выборке.
+*/
+namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Catalog catalog = new Catalog();
+            Program.Menu();
+            bool flag = true;
+            while (flag) {
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        {
+                            
+                            Console.WriteLine("Введите название книги: ");
+                            string name_of_book = Console.ReadLine();
+                            if (string.IsNullOrWhiteSpace(name_of_book))
+                            {
+                                throw new ArgumentNullException(nameof(name_of_book));
+                            }
+
+
+                            Console.WriteLine("Введите автора книги: ");
+                            string autor = Console.ReadLine();
+                            if (string.IsNullOrWhiteSpace(autor))
+                            {
+                                throw new ArgumentNullException(nameof(autor));
+                            }
+
+
+                            Console.WriteLine("Введите ISBN книги: ");
+                            int ISBN = int.Parse(Console.ReadLine());
+                            
+
+                            Console.WriteLine("Введите аннотацию книги: ");
+                            string annotation = Console.ReadLine();
+                            if (string.IsNullOrWhiteSpace(annotation))
+                            {
+                                throw new ArgumentNullException(nameof(annotation));
+                            }
+
+                            Console.WriteLine("Введите краткое содержание книги: ");
+                            string summary = Console.ReadLine();
+                            if (string.IsNullOrWhiteSpace(summary))
+                            {
+                                throw new ArgumentNullException(nameof(summary));
+                            }
+
+                            Console.WriteLine("Введите сожержание книги: ");
+                            string Text = Console.ReadLine();
+                            if (string.IsNullOrWhiteSpace(Text))
+                            {
+                                throw new ArgumentNullException(nameof(Text));
+                            }
+
+                            Book book = new Book(name_of_book, autor, ISBN, annotation, summary, Text);
+                            catalog.AddBook(book);
+                            
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Введите название книги: ");
+                            string name_of_book = Console.ReadLine();
+                            catalog.SearchByNameOfBook(name_of_book);
+                            
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Введите фрагмент книги: ");
+                            string fragment = Console.ReadLine();
+                            catalog.SearchByFragment(fragment);
+                            
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Введите ключевое слово: ");
+                            string KeyWord = Console.ReadLine();
+                            catalog.SearchByKeyWord(KeyWord);
+                            
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Введите имя автора: ");
+                            string autor = Console.ReadLine();
+                            catalog.SearchByAutor(autor);
+                            
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("Введите ISBN книги: ");
+                            int ISBN = int.Parse(Console.ReadLine());
+                            catalog.SearchByISBN(ISBN);
+                            
+                            break;
+                        }
+                    case 7:
+                        {
+                            flag = false;
+                            break;
+                        }
+
+                }
+                Program.Menu();
+            }
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("Выбирите действие :");
+            Console.WriteLine("1. добавить книгу в каталог");
+            Console.WriteLine("2. поиск по названию");
+            Console.WriteLine("3. поиск по фрагменту");
+            Console.WriteLine("4. поиск по ключевым словам");
+            Console.WriteLine("5. поиск по имени автора");
+            Console.WriteLine("6. поиск по ISBN");
+            Console.WriteLine("7. выход ");
+        }
+    }
+}
